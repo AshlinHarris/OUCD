14bdb676d622e3b1d63b9f299e34772f856449b4
Ashlin Harris <ashlin_harris@brown.edu>
2022-04-12 16:39:27 -0400

    Migrate remaining subroutines to external module (breaks main.f90 and old.f90)

--- standard.f90
+++ standard.f90
@@ -6,11 +6,127 @@ MODULE standard
 
 	INTEGER, parameter:: dp=kind(0.d0)
 
+	CONTAINS
+
+! --------------------------------------------------------------------
+! determine if a tiling exhibits complete dominance
+! by examining interior points of an m+2*x-2 by n+*2x-2 tiling
+! --------------------------------------------------------------------
+
+SUBROUTINE CHECK_COMPLETENESS(s,m,n,x,completeDominance)
+
+	IMPLICIT NONE
+
+	LOGICAL, INTENT(IN) :: s(:,:) ! augmented tile
+	INTEGER, INTENT(IN) :: m,n,x
+
+	LOGICAL, INTENT(OUT) :: completeDominance
+
+	INTEGER :: i,j,i1,j2 ! counters
+	LOGICAL :: dominated
+
+	INTEGER :: MAX_ROW, MAX_COLUMN
+
+	MAX_ROW	= m+2*x-1
+	MAX_COLUMN = n+2*x-1
+
+	! loop through each interior point
+	DO j=2,MAX_COLUMN
+	DO i=2,MAX_ROW
+
+		dominated = .false.
+
+		! loop through each adjacent point
+		DO j2=j-1,j+1
+		DO i1=i-1,i+1
+
+			! check for dominating nodes
+			! node cannot dominate self
+			IF (s(i1,j2) .and. .not. ( i1 .eq. i .and. j2 .eq. j)  ) THEN
+				dominated = .true.
+			END IF
+
+		END DO
+		END DO
+
+		! cancel the search if dominance is not complete
+		IF (.not. dominated) THEN
+			completeDominance = .FALSE.
+			GO TO 370
+		END IF
+
+	END DO
+	END DO
+
+	! each point is dominated
+	completeDominance = .TRUE.
+
+370	RETURN
+
+END SUBROUTINE CHECK_COMPLETENESS
+
+!---------------------------------------------------------------------------------------
+! check if every node's dominance is unique
+!---------------------------------------------------------------------------------------
+
+LOGICAL FUNCTION CHECK_UNIQUENESS(a,m,n,x,k) result(unique)
+
+	IMPLICIT NONE
+
+	INTEGER, INTENT(IN) :: a(:,:,:)
+	INTEGER, INTENT(IN) :: m,n,x,k
+	INTEGER :: i,j,i1,i2
+
+	INTEGER :: MAX_ROW, MAX_COLUMN
+
+	! Adjacencies are not accurate for exterior nodes.
+	! Only interior nodes of the augmented tile are considered.
+	! These are the only nodes stored in the adjacency lists.
+	! So, the size of the adjacency matrix is (k,m+2*x-2,n+2*x-2)
+	
+	MAX_ROW	= m+2*x-2
+	MAX_COLUMN = n+2*x-2
+
+	! for each node ...
+	DO j=1,MAX_COLUMN
+	DO i=1,MAX_ROW
+
+		! ... consider all subsequent nodes
+		! the loop is broken into 2 parts to avoid the case i = j
+		! first the nodes in the same column
+		i2=j
+		DO i1=i+1,MAX_ROW
+			IF ( INT_COMPARE(a(:,i,j), a(:,i1,i2),k) ) THEN
+				unique = .FALSE.
+				GO TO 39 ! return immediately
+			END IF
+		END DO				
+
+	   		! second, all others
+		DO i2=j+1,MAX_COLUMN
+		DO i1=1,MAX_ROW
+
+			IF ( INT_COMPARE(a(:,i,j), a(:,i1,i2),k) ) THEN
+				unique = .FALSE.
+				GO TO 39 ! return immediately
+			END IF
+
+		END DO
+		END DO
+
+	END DO
+	END DO
+
+	! since every adjacency was different, the dominance must be unique
+	unique = .TRUE.
+
+39	RETURN
+
+END FUNCTION CHECK_UNIQUENESS
+
 !---------------------------------------------------------------------------------------
 ! convert a coordinate to a node index
 !---------------------------------------------------------------------------------------
-
-	CONTAINS
 
 	SUBROUTINE COORD_TO_INDEX(i,n,x,y)
 
@@ -46,6 +162,91 @@ SUBROUTINE FORMAT_TIME(val,res)
 	res = (/ hours, minutes, seconds, milliseconds /)
 
 END SUBROUTINE FORMAT_TIME
+
+! --------------------------------------------------------------------
+! return the adjacency arrays of an m+x by n+x tile with
+! n_d dominating nodes and connectivity k
+! --------------------------------------------------------------------
+
+SUBROUTINE GENERATE_ADJACENCIES(m,n,k,a,x,d)
+
+	IMPLICIT NONE
+
+	INTEGER, INTENT(IN) :: m ! rows
+	INTEGER, INTENT(IN) :: n ! columns
+	INTEGER, INTENT(IN) :: k ! connectivity
+
+	! the adjacencies array uses coordinates from the augmented grid
+	INTEGER, INTENT(INOUT) :: a(:,:,:) ! adjacencies
+	LOGICAL, INTENT(IN) :: d(:,:) ! augmented tile
+
+	INTEGER :: i,j,i2,j2! counters
+	INTEGER :: MAX_ROW,MAX_COLUMN
+
+	INTEGER, INTENT(IN) :: x
+	!INTEGER, allocatable :: adjacent_nodes(:)
+	INTEGER :: index
+
+	MAX_ROW = m+2*x-2
+	MAX_COLUMN = n+2*x-2
+
+	! initialize adjacency arrays
+	a=0
+
+!$OMP PARALLEL SHARED(d,a,MAX_COLUMN,MAX_ROW,n,k) PRIVATE(i,j,i2,j2,index)
+!$OMP DO
+
+	! for each interior node of the augmented graph ...
+	DO j=1,MAX_COLUMN
+	DO i=1,MAX_ROW
+		
+		! ... record the indices of adjacent dominant nodes.
+		! Array indices for the space are off by one.
+
+		! nodes to the left
+		j2=j
+		DO i2=i,i+2
+			IF (d(i2,j2)) THEN
+				call COORD_TO_INDEX(index,n,i2,j2)
+				a(i2-i+1,i,j) = index
+			END IF
+		END DO
+
+		! node above
+		j2=j+1
+		i2=i
+		IF (d(i2,j2)) THEN
+			call COORD_TO_INDEX(index,n,i2,j2)
+			a(4,i,j) = index
+		END IF
+
+		! node below
+		j2=j+1
+		i2=i+2
+		IF (d(i2,j2)) THEN
+			call COORD_TO_INDEX(index,n,i2,j2)
+			a(5,i,j) = index
+		END IF
+
+		! nodes to the right
+		j2=j+2
+		DO i2=i,i+2
+			IF (d(i2,j2)) THEN
+				call COORD_TO_INDEX(index,n,i2,j2)
+				a(i2-i+6,i,j) = index
+			END IF
+		END DO
+
+		! INT_SORT the adjacencies to enable comparison
+		call INT_SORT(a(:,i,j),k)
+		
+	END DO
+	END DO
+
+!$OMP END DO
+!$OMP END PARALLEL
+
+END SUBROUTINE GENERATE_ADJACENCIES
 
 ! --------------------------------------------------------------------
 ! given a tile, return a section of the infinite tiling
@@ -378,6 +579,95 @@ SUBROUTINE PRINT_TILE( tile, m, n )
 	write (*,"(a)") '/'
 
 END SUBROUTINE PRINT_TILE
+
+!---------------------------------------------------------------------------------------
+! find a tiling that dominates all nodes uniquely and completely, if it exists
+! for a given size of tile and number of dominating nodes
+!---------------------------------------------------------------------------------------
+
+SUBROUTINE SEARCH_FOR_SOLUTION(d_nodes,m,n,n_d,solved)
+
+	IMPLICIT NONE
+
+	INTEGER, INTENT(IN) :: m ! rows in tile
+	INTEGER, INTENT(IN) :: n ! columns in tile
+	INTEGER, INTENT(IN) :: n_d
+
+	INTEGER, allocatable, INTENT(OUT) :: d_nodes(:)
+	LOGICAL, INTENT(OUT) :: solved
+
+	LOGICAL, allocatable :: g(:,:)     ! tile
+	LOGICAL, allocatable :: aug(:,:)   ! augmented tile
+	INTEGER, allocatable :: adj(:,:,:) ! adjacencies
+
+	INTEGER, parameter :: k = 8 ! connectivity of graph
+	INTEGER, parameter :: x = 2 ! border width
+
+	INTEGER :: ALLOCATED     ! status of allocation
+	INTEGER :: DEALLOCATED   ! status of deallocation
+	LOGICAL :: continue_flag ! can a solution still be found?
+	LOGICAL :: isDominated   ! does a set exhibit complete dominance?
+
+	allocate(d_nodes(n_d),stat=ALLOCATED)
+	IF (ALLOCATED /= 0) STOP "*** Not enough memory ***"
+
+	call GET_FIRST_SET(d_nodes,n_d)
+
+	continue_flag = .TRUE.
+	solved = .FALSE.
+
+	DO while (continue_flag)
+
+		allocate(g(m,n),stat=ALLOCATED)
+		IF (ALLOCATED /= 0) STOP "*** Not enough memory ***"
+
+		call LIST_TO_MATRIX(d_nodes,n_d,g,n)
+	
+		! create m+2x by n+2x graph containing g and a border
+		allocate(aug(m+2*x,n+2*x),stat=ALLOCATED)
+		IF (ALLOCATED /= 0) STOP "*** Not enough memory ***"
+
+		call GENERATE_TILING(g,m,n,x,aug)
+
+		!check for completeness
+		call CHECK_COMPLETENESS(aug,m,n,x,isDominated)
+
+		IF (isDominated) THEN
+
+			allocate(adj(k,m+2*x-2,n+2*x-2),stat=ALLOCATED)
+			IF (ALLOCATED /= 0) STOP "*** Not enough memory ***"
+
+			! check for CHECK_UNIQUENESS
+			call GENERATE_ADJACENCIES(m,n,k,adj,x,aug)
+
+			IF (CHECK_UNIQUENESS(adj,m,n,x,k)) THEN
+				solved = .TRUE.
+			ELSE
+				solved = .FALSE.
+			END IF
+
+			deallocate(adj,stat=DEALLOCATED)
+			IF (DEALLOCATED /= 0) STOP "*** Could not deallocate ***"
+
+		ELSE
+			solved = .FALSE.
+		END IF
+
+		IF(solved) THEN
+			continue_flag = .FALSE.
+		ELSE
+			! try to update d_nodes
+			call GET_NEXT_SET(d_nodes,n_d,m,n,continue_flag)
+		END IF
+
+		deallocate(g,stat=DEALLOCATED)
+		IF (DEALLOCATED /= 0) STOP "*** Could not deallocate ***"
+		deallocate(aug,stat=DEALLOCATED)
+		IF (DEALLOCATED /= 0) STOP "*** Could not deallocate ***"
+
+	END DO
+
+END SUBROUTINE SEARCH_FOR_SOLUTION
 
 END MODULE standard
 
